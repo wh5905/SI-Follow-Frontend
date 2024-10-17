a239a20724c3fff746d7f76b0db0e8d221bef246
jung won hyeong <145119366+wh5905@users.noreply.github.com>
2024-10-17 16:20:35 +0900

    ssr을 위한 정적파일생성 제거.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
     images: { unoptimized: true },
     trailingSlash: true,
-    exportPathMap: () => ({
-        "/": { page: "/" },
-        "/chat": { page: "/chat" },
-        "/github/github-access-token": { page: "/github/github-access-token" },
-    }),
 };
 
 export default nextConfig;
